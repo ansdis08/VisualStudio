42a482d3b26f3b303bebed9bf0eafc213267f9aa
Jana <janausachova16@gmail.com>
2024-12-16 14:13:02 +0000

    Add folder 12_09

---
+++ 12_09/12_09/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace _12_09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N=";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "3.30";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 72);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(219, 408);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(306, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "3.29";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(306, 163);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 43);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "3.44";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(306, 229);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 43);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "3.";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(306, 279);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 47);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "3.41";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 583);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ 12_09/12_09/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12_09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] NatKva(int N)
+        {
+            if(N<0) { N= 0; }
+            int[] A = new int[N];
+            for (int i = 0; i<N; i++)
+                A[i] = (i+1)*(i+1);
+            return A;
+        }
+
+        string ToStr(int[] A) 
+        {   //kopiga
+            string s = " ";
+            for (int i = 0; i < A.Length; i++)
+                s+= A[i]+"\n";
+                return s;
+        }
+        //3.30 Izveidojiet funkciju int[] NatKva(int N),
+        //kuras atgriežamā vērtība ir pirmo N naturālu skaitļu
+        //kvadrātu masīvs: (1, 4, 9, 16, 25, 36...).
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int N= int.Parse(textBox1.Text);
+            int[] A = NatKva(N);
+            richTextBox1.Text= "A: \n" + ToStr(A);
+        }
+
+        int[] Prime(int N) 
+        {
+            if (N < 0) { N = 0; }
+            int[] A = new int[N];
+            int NN = 0;
+            if (N > 0) { A[0] = 2; NN++; }
+            int k = 1;
+            while (NN < N)
+            {
+                k += 2;
+                bool IsPrime = true;
+                for (int i = 0; i < NN; i++)
+                {
+                    if (k % A[i] == 0) { IsPrime = false; break; }
+                }
+                    if (IsPrime) { A[NN] = k; NN++; }
+               
+            }
+            return A;
+        }
+
+        //3.29 Izveidojiet funkciju int[] Prime(int N),
+        //kuras atgriežamā vērtība ir pirmo
+        //N pirmskaitļu masīvs (2, 3, 5, 7, 11, 13...).
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int N = int.Parse(textBox1.Text);
+            int[] A = Prime(N);
+            richTextBox1.Text = "A: \n" + ToStr(A);
+        }
+
+        double Laiks(double h, double g = 9.8)
+        {
+            return Math.Sqrt(2*h/g);
+        }
+        //3.44 a
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.AppendText("Zeme: " + Laiks(12).ToString("F2")+"\n");
+            richTextBox1.AppendText("Men: " + Laiks(12, 1.8).ToString("F2") + "\n");
+        }
+
+        double t(double h)
+        {
+            return Math.Sqrt(2 * h / 9.8);
+        }
+
+        double t(double h, double g)
+        {
+            return Math.Sqrt(2 * h / g);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.AppendText("Zeme: " + Laiks(12).ToString("F2") + "\n");
+            richTextBox1.AppendText("Men: " + Laiks(12, 1.8).ToString("F2") + "\n");
+        }
+
+
+        //3.41
+        double Av(params int[] A) 
+        { 
+            int N=A.Length;
+            int S = 0;
+            for (int i = 0; i < N; i++) S += A[i];
+            return S*1.0/N;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.AppendText("Av(2, 5)=" + Av(2, 5) + "\n");
+            richTextBox1.AppendText("Av(1,2,3,4, 5)=" + Av(1, 2, 3, 4, 5) + "\n");
+        }
+    }
+}
