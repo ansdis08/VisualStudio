fe5de664122555bc92bb05cd8ed93c0e8b487e89
Jana <janausachova16@gmail.com>
2024-12-16 14:20:11 +0000

    Add folder 2609

---
+++ 2609/2609/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace _2609
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(33, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(491, 462);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(551, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "uzd 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(551, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "uzd 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(551, 100);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "uzd 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(551, 141);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 33);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "uzd 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(551, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "N=";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(582, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "5";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(554, 239);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(79, 35);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "uzd 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ 2609/2609/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2609
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double y(double x) 
+        { 
+         return (x - x *x + 1)/(Math.Sqrt(2*x*x+1)+2);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "  x       y \n";
+            for(double x = -10; x<=10; x+=0.5)
+                richTextBox1.AppendText(" "+x.ToString("F2")+ " \t" +y(x).ToString("F2") +"\n");
+        }
+
+        int RecN(int n)
+        {
+            if (n <= 1) return 1;
+            return RecN(n-1)*RecN(n-1)+ RecN(n - 2) * RecN(n - 2);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.AppendText("RecN(1)=" + RecN(1) + "\n");
+            richTextBox1.AppendText("RecN(5)=" + RecN(5) + "\n");
+            richTextBox1.AppendText("RecN(6)=" + RecN(6) + "\n");
+        }
+
+        int TriCip(params int[] a)
+        {
+            int x = 0;
+            for (int i = 0; i < a.Length; i++) 
+                if (Math.Abs(a[i]) >=100 && Math.Abs(a[i])<=999) x++;
+            return x;
+
+         }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.AppendText("TriCip(1, 111, 225, 1000)=" + TriCip(1, 111, 225, 1000) + "\n");
+            richTextBox1.AppendText("TriCip(-1, -111, -225, -1000)=" + TriCip(-1, -111, -225, -1000) + "\n");
+        }
+
+        string[] DoArr(int N)
+        {
+            string[] A = new string[N];
+            Random r = new Random();
+            for (int i = 0; i < N; i++)
+                A[i] = "" + (char)(r.Next('A','Z' + 1)) + (r.Next('A', 'Z' + 1)) + (r.Next('A', 'Z' + 1));
+            return A;
+        }
+
+        string ArrToStr(string[] s)
+        {
+            string x = "";
+            for(int i = 0;i < s.Length;i++) x += s[i]+"\n";
+            return x;
+        }
+
+        void Cript(string[] s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                string t = "";
+                for (int k = 0; k < 3; k++)
+                    if (s[i][k] == 'Z') t += 'A';
+                    else t+= (char)(s[i][k] + 1);
+                s[i] = t;
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int N = int.Parse(textBox1.Text);
+            string[] A = DoArr(N);
+            richTextBox1.Text = ArrToStr(A);
+            Cript(A);
+            richTextBox1.AppendText("\n Cript: \n");
+            richTextBox1.AppendText(ArrToStr(A));
+        }
+
+        bool Y(double x, out double y)
+        {
+            double xr = x * Math.PI / 180;
+            if (Math.Abs(Math.Cos(xr))<0.0001)
+            {
+                y = double.MaxValue;
+                return false;
+            }
+            else {
+                y = (1+Math.Sin(xr))/Math.Cos(xr);
+                return true;
+
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double y;
+            richTextBox1.Text = "  x       y \n";
+            for (double x = 0; x <= 360; x += 15)
+                if(Y(x,out  y))
+                richTextBox1.AppendText(" " + x.ToString("F0") + " \t" + y.ToString("F0") + "\n");
+            else richTextBox1.AppendText(" " + x.ToString("F0") + " \t - \n");
+        }
+    }
+}
