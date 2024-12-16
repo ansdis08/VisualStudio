4c5d1e7e9ff4708d0a255dd5de986389f2f5b152
Jana <janausachova16@gmail.com>
2024-12-16 15:12:37 +0000

    add 1710

---
+++ 1710/1710/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1710
+{
+    public class Velocity
+    {
+
+
+        private double sec, m;
+
+            public double Sec
+            {
+                get { return sec; }
+                set { sec = value; }
+            }
+
+            public double Min
+            {
+                get { return sec / 60; }
+                set { sec = value * 60; }
+            }
+
+            public double H
+            {
+                get { return sec / 3600.0; }
+                set { sec = value * 3600; }
+            }
+
+            public double M
+            {
+                get { return m; }
+                set { m = value; }
+            }
+
+            public double Km
+            {
+                get { return m / 1000; }
+                set { m = value * 1000; }
+            }
+
+            public Velocity()
+            {
+                sec = 0; m = 0;
+            }
+
+        public double MSec()
+        {
+
+            return m / sec;
+
+        }
+
+
+        public double KmH()
+        {
+
+            //return (m/1000)/(sec/36000);
+            return 3.6*m /sec;
+
+        }
+
+    }
+}
---
+++ 1710/1710/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1710
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = System.Drawing.Color.Aqua;
+            Velocity v1 = new Velocity();
+            if(comboBox1.Text=="m") v1.M = double.Parse(textBox1.Text);
+            else if(comboBox1.Text=="km") v1.Km = double.Parse(textBox1.Text);
+
+            if (comboBox2.Text == "sec") v1.Sec = double.Parse(textBox2.Text);
+            else if (comboBox2.Text == "min") v1.Min = double.Parse(textBox2.Text);
+            else if (comboBox2.Text == "h") v1.H = double.Parse(textBox2.Text);
+
+            richTextBox1.Clear();
+            richTextBox1.AppendText("\n H=" + v1.H);
+            richTextBox1.AppendText("\n Min=" + v1.Min);
+            richTextBox1.AppendText("\n Sec=" + v1.Sec);
+
+            richTextBox1.AppendText("\n M=" + v1.M);
+            richTextBox1.AppendText("\n Km=" + v1.Km);
+
+            richTextBox1.AppendText("\n m/sec=" + v1.MSec());
+            richTextBox1.AppendText("\n km/h=" + v1.KmH());
+        }
+    }
+}
